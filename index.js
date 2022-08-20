cb9286a4285639fae10b165ef06771c42c8c9971
wanjiku <annritawanjiku77@gmail.com>
2022-08-20 19:36:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+const leastFavoriteCustomer = "ann";
+
+function upperCaseCustomerName(){
+
+    // let customer= "bob";
+
+    customerName=customerName.toUpperCase();
+
+    // return customer;
+}
+
+upperCaseCustomerName();
+
+
+console.log(upperCaseCustomerName("bob"));
+
+function setBestCustomer(){
+    
+    bestCustomer = "not bob"
+
+    return bestCustomer;
+
+}
+setBestCustomer();
+
+function overwriteBestCustomer(){
+    bestCustomer ="maybe bob";
+
+    return bestCustomer;
+}
+
+overwriteBestCustomer();
+
+
+
+function changeLeastFavoriteCustomer(){
+
+    leastFavoriteCustomer = "jane";
+
+    return leastFavoriteCustomer;
+
+}
+
+changeLeastFavoriteCustomer();
